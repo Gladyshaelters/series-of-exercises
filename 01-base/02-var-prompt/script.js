61556061215e3b5a83aeef575daf0e2dd4019679
Gladys Haelters <haelters_gladys@hotmail.com>
2022-06-15 12:29:42 +0200

    Made exerc 1 and 2

--- 01-base/02-var-prompt/script.js
+++ 01-base/02-var-prompt/script.js
@@ -1,8 +1,7 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
+let name = prompt("What is your name?");
 
-(function() {
 
-    // your code here
-
-})();
+alert("Hello, " + name + "!")
+(function () {})();
